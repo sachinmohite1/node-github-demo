fb88dc9eeefc8134ab52c056b06762a5b651c163
Immanuel-john <imjjohn18@gmail.com>
2021-09-27 09:29:54 +0530

    updated the response text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
     // Respond with "hello world" for requests that hit our root "/"
     app.get("/", function (req, res) {
-     return res.send("Hello World github actions");
+     return res.send("Hello World github actions hi");
     });
 
     // listen to port 7000 by default
